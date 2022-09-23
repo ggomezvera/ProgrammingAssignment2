2cc6406bf8b58ed822fad5c7a658d6f0ef3487a0
ggomezvera <113449136+ggomezvera@users.noreply.github.com>
2022-09-23 16:17:17 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+rm(list = ls())
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(ma = matrix()) {
+  im <- NULL
+  setMatrix <- function(y) {
+    ma <<- y
+    im <<- NULL
+  }
+  getMatrix <- function() ma
+  setinverse <- function(inv) im <<- inv
+  getinverse <- function() im
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getinverse()
+  if (!is.null(im)) {
+    message("getting cached inverse matrix")
+    return(im)
+  }
+  data <- x$getMatrix()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+##testing the functions
+B <- matrix(c(1,2,3,4),2,2)
+
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1) #inverse returned after computation, no message 
+
+cacheSolve(B1) #inverse returned from cache and message is printed here
+
+B2 <- makeCacheMatrix(-B)
+cacheSolve(B1)
+cacheSolve(B2)
